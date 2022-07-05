a7b36c100003cd7dac66ce69f0dd2ec575ea6f21
Luciano Santos <luciano.ssants@gmail.com>
2022-07-05 18:04:26 -0300

    limit pool size

--- internal/infra/database/mongo.go
+++ internal/infra/database/mongo.go
@@ -22,7 +22,7 @@ func (d MongoDriver) GetClient() (*mongo.Client, error) {
 
 	defer cancel()
 
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(d.connectionString).SetMaxConnecting(10).SetMaxPoolSize(10))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(d.connectionString).SetMaxConnecting(3).SetMaxPoolSize(3))
 
 	if err != nil {
 		return nil, err
